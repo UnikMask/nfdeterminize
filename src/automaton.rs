7ba5d7bd9072dfe2798265421792dc32156936e7
apjg1 <apjg1@st-andrews.ac.uk>
2022-11-01 18:46:05 +0000

    [automaton] Separated rabin scott algo to method

--- src/automaton.rs
+++ src/automaton.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 
+use crate::trie::FrontierController;
 use crate::trie::NodeTrie;
+use crate::trie::Seq;
 
 #[derive(Debug)]
 pub enum AutomatonType {
@@ -37,6 +41,68 @@ impl Automaton {
             end,
         }
     }
+
+    // Rabin Scott Superset Construction Algorithm
+    fn rabin_scott(&self) -> (Vec<usize>, Vec<usize>, Vec<(usize, usize, usize)>) {
+        // Rabin Scott Superset Construction Algorithm
+        let mut controller = FrontierController::new();
+        let mut state_trie = NodeTrie::new(&mut controller, true); // Empty sequence/set is a state.
+        let mut transitions: Vec<(usize, usize, usize)> = Vec::new();
+
+        // Insert start state
+        controller.frontier.push_front(Seq::from(&self.start));
+
+        // Loop add to trie until frontier is empty
+        'main: loop {
+            let mut state_set: Seq = match controller.frontier.pop_front() {
+                Some(mut set) => set,
+                None => break 'main,
+            };
+            // Populate sets for each alphabet symbols
+            let mut symbol_sets: Vec<HashSet<usize>> = Vec::new();
+            for _ in 0..self.alphabet {
+                symbol_sets.push(HashSet::new());
+            }
+
+            let start_state = match state_trie.get_addr(&state_set, &mut controller) {
+                Some(address) => address,
+                None => panic!("Start state does not have an address!"),
+            };
+
+            // Populate symbols set to get each set of states for each symbol
+            while let Seq::Cons(next, rest) = state_set {
+                for transition in &self.table {
+                    if transition.0 == next {
+                        symbol_sets[transition.1].insert(transition.2);
+                    }
+                }
+                state_set = *rest;
+            }
+
+            // For each sets of states, populate the state trie, get a number, and populate
+            // the transition map.
+            'symbolRec: for i in 0..self.alphabet {
+                let current_set = match symbol_sets.get(i) {
+                    Some(set) => set,
+                    None => break 'symbolRec,
+                };
+                let seq = Seq::from(current_set);
+
+                // Check if the new sequence is new. If it is, add it to map and frontier.
+                if let None = state_trie.get(&seq) {
+                    state_trie.push(&seq, &mut controller);
+                }
+                // Get the sequence's address and the new transition to the transitions map.
+                let end_state = match state_trie.get_addr(&seq, &mut controller) {
+                    Some(address) => address,
+                    None => panic!("Node address should have been assigned!"),
+                };
+                transitions.push((start_state, i, end_state));
+            }
+        }
+        return (self.start, self.end, transitions);
+    }
+
     // Determinize an NFA.
     fn determinize(&self) -> Automaton {
         let mut ret = Automaton {
@@ -49,25 +115,33 @@ impl Automaton {
         };
 
         // Return same automaton as it already is deterministic.
-        if let AutomatonType::Det = self.automaton_type  {
+        let ret = match self.automaton_type {
+            AutomatonType::Det => Automaton {
+                automaton_type: AutomatonType::Det,
+                size: self.size ,
+                alphabet: self.alphabet,
+                table: self.table.clone(),
+                start: self.start.clone(),
+                end: self.end.clone()
+            },
+            AutomatonType::NonDet => {
+                let (transitions, start, end) = self.rabin_scott();
+                return Automaton {
+                    automaton_type: AutomatonType::Det,
+                    size: transitions.size(),
+                    alphabel: self.alphabet,
+                    table: transitions,
+                    start: start,
+                    end: end
+                };
+            }
+        }
+        if let AutomatonType::Det = self.automaton_type {
             ret.table = self.table.clone();
             ret.start = self.start.clone();
-            ret.end  = self.end.clone();
+            ret.end = self.end.clone();
             return ret;
         }
-
-        // Rabin Scott Superset Construction Algorithm
-        let mut state_trie = NodeTrie::new(&self.size);
-        let frontier: VecDeque<Vec<usize>> = VecDeque::new();
-
-        if state_trie.push_seq(self.start.clone()) == true {
-
-        }
-
-
-
-
-
         return ret;
     }
 
--- src/trie.rs
+++ src/trie.rs
@@ -1,46 +1,138 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
+pub enum Seq {
+    Cons(usize, Box<Seq>),
+    Nil,
+}
 
+impl From<&Vec<usize>> for Seq {
+    fn from(v: &Vec<usize>) -> Self {
+        let mut head = Seq::Nil;
+
+        for &n in v {
+            head = Seq::Cons(n, Box::new(head));
+        }
+        return head;
+    }
+}
+
+impl From<&HashSet<usize>> for Seq {
+    fn from(s: &HashSet<usize>) -> Self {
+        let mut v = Vec::from_iter(s);
+        v.sort();
+
+        let mut head = Seq::Nil;
+        for n in v {
+            head = Seq::Cons(*n, Box::new(head));
+        }
+        return head;
+    }
+}
 
 pub struct NodeTrie {
-    size: usize,
     children: HashMap<usize, NodeTrie>,
+    state_num: Option<usize>,
+}
+
+pub struct FrontierController {
+    pub frontier: VecDeque<Seq>,
+    counter: usize,
+}
+
+impl FrontierController {
+    pub fn new() -> Self {
+        Self {
+            frontier: VecDeque::new(),
+            counter: 0,
+        }
+    }
 }
 
 impl NodeTrie {
-    pub fn new(n: &usize) -> NodeTrie {
-        NodeTrie {
-            size: *n,
+    pub fn new(controller: &mut FrontierController, assign: bool) -> Self {
+        controller.counter += if assign { 1 } else { 0 };
+        Self {
             children: HashMap::new(),
+            state_num: if assign {
+                Some(controller.counter.clone() - 1)
+            } else {
+                None
+            },
         }
     }
 
-    pub fn push(&mut self, node: usize) -> bool {
-        if !self.children.contains_key(&node) {
-            self.children.insert(node, NodeTrie::new(&self.size));
-            true
-        } else {
-            false
-        }
-    }
+    pub fn push(&mut self, sequence: &Seq, controller: &mut FrontierController) -> bool {
+        match sequence {
+            Seq::Cons(next, rest) => match self.children.get_mut(&next) {
+                Some(c) => c.push(&rest, controller),
+                None => {
+                    // Set new counter number
+                    let mut new_trie =
+                        NodeTrie::new(controller, if let Seq::Nil = **rest { true } else { false });
 
-    pub fn push_seq (mut self, nodes: Vec<usize>) -> bool {
-        let mut is_new = false;
-        let mut trie = &self;
-        let mut node_ptr = 0;
-        return is_new;
-    }
-
-    pub fn contains(&self, seq: &Vec<usize>) -> bool {
-       if seq.len() == 0 {
-            return true;
-       } else {
-            match &self.children.get(&seq[0]) {
-                Some(mut trie) => {
-                    return trie.contains(&seq[1..].to_vec());
-                },
-                None => return false,
+                    new_trie.push(&rest, controller);
+                    self.children.insert(*next, new_trie);
+                    return false;
+                }
+            },
+            Seq::Nil => {
+                return true;
             }
-       } 
+        }
+    }
+
+    pub fn get(&mut self, seq: &Seq) -> Option<&mut NodeTrie> {
+        match seq {
+            Seq::Nil => {
+                return Some(self);
+            }
+            Seq::Cons(next, rest) => match self.children.get_mut(&next) {
+                Some(child) => {
+                    return child.get(&rest);
+                }
+                None => {
+                    return None;
+                }
+            },
+        }
+    }
+
+    pub fn get_addr(&mut self, seq: &Seq, controller: &mut FrontierController) -> Option<usize> {
+        match seq {
+            Seq::Nil => {
+                if let Some(address) = self.state_num {
+                    return Some(address);
+                } else if let None = self.state_num {
+                    let address = controller.counter - 1;
+                    self.state_num = Some(address);
+                    controller.counter += 1;
+                    return Some(address);
+                }
+            }
+            Seq::Cons(next, rest) => match self.children.get_mut(&next) {
+                Some(child) => {
+                    return child.get_addr(&rest, controller);
+                }
+                None => {
+                    return None;
+                }
+            },
+        }
+    }
+
+    pub fn contains(&self, seq: &Seq) -> bool {
+        match seq {
+            Seq::Nil => {
+                return true;
+            }
+            Seq::Cons(next, rest) => match self.children.get(&next) {
+                Some(child) => {
+                    return child.contains(&rest);
+                }
+                None => {
+                    return false;
+                }
+            },
+        }
     }
 }
