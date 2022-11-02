11335ca5f7a83e63cde8a5cfea4702b45de95973
apjg1 <apjg1@st-andrews.ac.uk>
2022-11-02 17:52:07 +0000

    [minimization][wip] Hopcroft Algo implementation

--- src/automaton.rs
+++ src/automaton.rs
@@ -50,8 +50,14 @@ impl Automaton {
         let mut transitions: Vec<(usize, usize, usize)> = Vec::new(); // All DFA transitions
         let mut accept_states: Vec<usize> = Vec::new(); // All accept states
 
-        // Insert start state
-        controller.frontier.push_front(Seq::from(&self.start));
+        // Insert start state in trie and frontier
+        let s0 = Seq::from(&self.start);
+        state_trie.push(&s0, &mut controller);
+        let s0_addr = match state_trie.get_addr(&s0, &mut controller) {
+            Some(address) => address,
+            None => panic!("Start state is supposed to have an address!"),
+        };
+        controller.frontier.push_front(s0);
 
         // Loop add to trie until frontier is empty
         'main: loop {
@@ -117,16 +123,11 @@ impl Automaton {
                 transitions.push((start_state, i, end_state));
             }
         }
-        return (
-            transitions,
-            controller.size(),
-            self.start.clone(),
-            accept_states,
-        );
+        return (transitions, controller.size(), vec![s0_addr], accept_states);
     }
 
     // Determinize an NFA.
-    fn determinize(&self) -> Automaton {
+    fn determinize(self) -> Automaton {
         // Return same automaton as it already is deterministic.
         let ret = match self.automaton_type {
             AutomatonType::Det => Automaton {
@@ -153,7 +154,21 @@ impl Automaton {
     }
 
     // Minimize a DFA.
-    fn minimize(&self) -> Automaton {
+    fn minimize(self) -> Automaton {
+        if let AutomatonType::Det = self.automaton_type {
+            return self;
+        }
+        // Get vector with all accept states removed.
+        let mut pk: Vec<Vec<usize>> = Vec::new();
+        let mut last = 0;
+        pk.push(Vec::new());
+        for end in &self.end {
+            pk[0].append(&mut (last..*end).collect());
+            last = end + 1;
+        }
+        pk[0].append(&mut (last..self.size).collect());
+        pk.push(self.end.clone());
+
         return Automaton {
             automaton_type: AutomatonType::Det,
             size: self.size,
@@ -163,4 +178,63 @@ impl Automaton {
             end: Vec::new(),
         };
     }
+
+    fn hopcroft_step(&self, p: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+        let mut p_next: Vec<Vec<usize>> = Vec::new();
+
+        // Get transition map for efficiency
+        let map = self.get_transition_map();
+
+        // Iterate for each set
+        for i in 0..p.len() {
+            let set = match p.get(i) {
+                Some(s) => s,
+                None => break,
+            };
+
+            // Iterate over each pair of the set
+            for j in 0..set.len() - 1 {
+                for k in j + 1..set.len() {
+                    let mut distinguish = false;
+                    for s in 0..self.alphabet {
+                        if get_set(map[&(set[j], s)], p) != get_set(map[&(set[k], s)], p) {
+                            distinguish = true;
+                            break;
+                        }
+                    }
+
+                    if distinguish {
+                        // Let j and k into 2 different sets
+                    }
+                }
+            }
+        }
+
+        return p_next;
+    }
+
+    fn get_transition_map(&self) -> HashMap<(usize, usize), usize> {
+        let mut map: HashMap<(usize, usize), usize> = HashMap::new();
+
+        for transition in &self.table {
+            map.insert((transition.0, transition.1), transition.2);
+        }
+
+        return map;
+    }
+}
+
+fn get_set(state: usize, p: &Vec<Vec<usize>>) -> Option<usize> {
+    // Iterate for each set
+    for i in 0..p.len() {
+        for s in match p.get(i) {
+            Some(s) => s,
+            None => break,
+        } {
+            if state == *s {
+                return Some(i);
+            }
+        }
+    }
+    return None;
 }
