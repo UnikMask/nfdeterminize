e07cdc69203c46c25f6e496d871b856a882ab6da
apjg1 <apjg1@st-andrews.ac.uk>
2023-01-18 17:09:13 +0000

    [main] Use NonDet DFA and calling algorithms.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use automaton::AutomatonType;
 
 fn main() {
     let dfa = Automaton::new(
-        AutomatonType::Det,
+        AutomatonType::NonDet,
         2,
         2,
         vec![(0, 0, 1), (0, 1, 0), (1, 0, 1), (1, 1, 0)],
         vec![0],
         vec![1],
     );
-    println!("Hello, {:?}", dfa);
+    println!("Hello, {:?}", dfa.determinized().minimized());
 }
