78684fcffb9606b7ca8cf2ddeaf1b246f6b954bf
apjg1 <apjg1@st-andrews.ac.uk>
2023-02-01 19:00:36 +0000

    Adapted encoder to GAP's automaton encoding.

--- src/automaton_encoder.rs
+++ src/automaton_encoder.rs
@@ -24,19 +24,35 @@ impl From<&String> for Automaton {
 
                 // Set size and alphabet.
                 ret.set_size(str::parse(contents.next().unwrap().as_str()).unwrap());
-                ret.set_alphabet(str::parse(contents.next().unwrap().as_str()).unwrap());
+                let alphabet_parse = contents.next().unwrap();
+                match alphabet_parse.as_rule() {
+                    Rule::LETTER_STR => {
+                        ret.set_alphabet(alphabet_parse.as_str().len());
+                    }
+                    Rule::NUM => {
+                        ret.set_alphabet(str::parse(alphabet_parse.as_str()).unwrap());
+                    }
+                    _ => {
+                        panic!("Unreachable code!");
+                    }
+                }
 
                 // Set transitions
-                let mut table: Vec<(usize, usize, usize)> = Vec::new();
-                for t in contents.next().unwrap().into_inner() {
-                    let mut t_inner = t.into_inner();
-                    table.push((
-                        str::parse(t_inner.next().unwrap().as_str()).unwrap(),
-                        str::parse(t_inner.next().unwrap().as_str()).unwrap(),
-                        str::parse(t_inner.next().unwrap().as_str()).unwrap(),
-                    ));
+                let mut tuple_table: Vec<(usize, usize, usize)> = Vec::new();
+                for (i_a, a) in contents
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .into_iter()
+                    .enumerate()
+                {
+                    for (i_s, s_in) in a.into_inner().into_iter().enumerate() {
+                        for s_out in s_in.into_inner() {
+                            tuple_table.push((i_s, i_a + 1, str::parse(s_out.as_str()).unwrap()));
+                        }
+                    }
                 }
-                ret.set_transitions(table);
+                ret.set_transitions(tuple_table);
 
                 // Set start states.
                 let mut start: Vec<usize> = Vec::new();
