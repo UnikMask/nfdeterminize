22c85f73160ffd9c7b926f8e71aa9ace36389e16
UnikMask <alexandre.guerville@hotmail.com>
2023-03-20 09:53:02 +0000

    [benches] Introduce constants for bns numbers

--- benches/automaton_benches.rs
+++ benches/automaton_benches.rs
@@ -1,11 +1,14 @@
-use std::time::Duration;
+use std::{ops::Range, time::Duration};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use nfdeterminize::transition_graphs::get_buffer_and_stack_aut;
 
+const NUM_BUFFERS: Range<usize> = 1..4;
+const NUM_STACKS: Range<usize> = 1..8;
+
 fn determinization_bns_benchmark(c: &mut Criterion) {
-    for i in 1..4 {
-        for j in 1..8 {
+    for i in NUM_BUFFERS {
+        for j in NUM_STACKS {
             let mut automaton = get_buffer_and_stack_aut(i, j);
             c.bench_function(&format!("determinize bns {i} {j}"), |b| {
                 b.iter(|| automaton = automaton.determinized())
